854a40449bbc03a40a6e2ba50ae8e505e95398a4
Laura Machado Antunes Santos <lauramachadosscontato@gmail.com>
2026-03-26 15:20:44 -0300

    integra uso da classe avaliação

--- ScreenSound/Modelos/Avaliacao.cs
+++ ScreenSound/Modelos/Avaliacao.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace ScreenSound.Modelos;
 
-namespace ScreenSound.Modelos
+internal class Avaliacao
 {
-    internal class Class1
+    public Avaliacao(int nota)
     {
+        Nota = nota;
     }
+
+public int Nota { get; }
 }
+
